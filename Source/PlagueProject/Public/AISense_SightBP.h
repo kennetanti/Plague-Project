5b4ce8738d1acef5f6f9c2c97b7a4399eca54c6e
s11101133 <s11101133@stu.edu.tw>
2018-06-10 10:40:06 +0800

    [更新] =====AISense Class更新=====
    .整合AISense_Sight的所有子Class，之後只要寫在一個.h內就好

---
+++ Source/PlagueProject/Public/AISense_SightBP.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AISense_Sight.h"
+#include "AISense_SightBP.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS(BlueprintType, Blueprintable)
+class PLAGUEPROJECT_API UAISense_Sight_Battle : public UAISense_Sight
+{
+	GENERATED_BODY()
+	
+};
+
+
+UCLASS()
+class PLAGUEPROJECT_API UAISense_Sight_Patrol : public UAISense_Sight
+{
+	GENERATED_BODY()
+
+};
+
