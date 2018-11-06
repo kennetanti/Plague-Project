5dae8c024a0560248b4d66e71c19894591caf13c
s11101133 <s11101133@stu.edu.tw>
2018-11-06 17:09:01 +0800

    更新 介面

---
+++ Source/PlagueProject/UI/RichText/MyRichTextBlockImageDecorator.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/RichTextBlockImageDecorator.h"
+#include "MyRichTextBlockImageDecorator.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class PLAGUEPROJECT_API UMyRichTextBlockImageDecorator : public URichTextBlockImageDecorator
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
